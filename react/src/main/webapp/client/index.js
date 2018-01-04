91bef225936c0e4ef88c81651b17be67e2a9593d
Johan Cronsioe <johan.cronsioe@gmail.com>
2018-01-04 20:19:06 +0100

    using helvetica font

--- react/src/main/webapp/client/index.js
+++ react/src/main/webapp/client/index.js
@@ -13,6 +13,8 @@ import history from './reducers/history.js';
 import ranking from './reducers/ranking.js';
 import notification from './reducers/notification.js';
 import algorithm from './reducers/algorithm.js';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -26,9 +28,14 @@ let reducers = combineReducers({
     algorithm,
 });
 let store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+const muiTheme = getMuiTheme({
+    fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif'
+});
+
 ReactDOM.render(
     <Provider store={store}>
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiTheme}>
             <div className={style.container}>
                 <App/>
             </div>
